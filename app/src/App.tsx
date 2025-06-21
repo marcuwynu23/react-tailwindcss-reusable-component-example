3f9fce8182e38fb10e072df3a239b181438a1cf4
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2025-06-22 04:12:09 +0800

    chore: implement components like toast and modal

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,31 @@
-import {Button} from "ui";
+import { useCallback, useState } from "react";
+import { Button, Container, Modal, useModal, useToast } from "ui";
+
 function App() {
+  const { isOpen, openModal, closeModal } = useModal();
+  const toast = useToast();
+
+  const [counter, setCounter] = useState(0);
+  const handleBtnClick = useCallback(() => {
+    openModal();
+    console.log("Hello World!");
+    setCounter(counter + 1);
+  }, [counter]);
+
   return (
     <>
-      <Button>Click Me</Button>
-      <p>Hello</p>
+      <Container className="h-screen w-screen" alignX="center" alignY="center">
+        <Container className="bg-green-600 rounded-full mt-6 py-6" alignX="center" alignY="center">
+          <p className="text-[2rem] font-bold text-white">{counter}</p>
+        </Container>
+        <Button className="mt-4" onClick={handleBtnClick}>
+          Click Me!
+        </Button>
+        <Button onClick={() => toast("info", "Registration Processing...", "top-right")}>Show Toast</Button>
+        <Modal isOpen={isOpen} onClose={closeModal} title="Registration Form" fullScreen>
+          <p className="mb-4">This is the modal content.</p>
+        </Modal>
+      </Container>
     </>
   );
 }
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,14 @@
-import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
-import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ToastContainer, ToastProvider } from "ui";
 import App from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ToastProvider>
+      <App />
+      <ToastContainer />
+    </ToastProvider>
   </StrictMode>
 );
